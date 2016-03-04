1f8ab66af4f2f92ea9659a1a8e901f7a7ab6e189
bantel <daniel.marvin@arcor.de>
2016-03-04 16:17:03 +0100

    Added files via upload

--- statistik.php
+++ statistik.php
@@ -1,5 +1,5 @@
 <?php
-include_once("/config.php");
+include_once("/db.php");
 
 #Variablen
 #=============================================================
@@ -31,68 +31,8 @@ else
 {
 	$vorbelastung = ""
 }
-#=============================================================
-function getNebenwirkungen(){
-	$tmp = mysql_query("SELECT `Name` FROM `Nebenwirkung`;");
-	while($row = mysql_fetch_object($tmp)){
-		$Name[] = $row;
-	}
-	
-	return $Name;
-}
-
-function getMedikamente(){
-	$tmp = mysql_query("SELECT `Name` FROM `Medikamente`;");
-	while($row = mysql_fetch_object($tmp)){
-		$Name[] = $row;
-	}
-	
-	return $Name;
-}
-
-function getPerson($alter, $geschlecht, $ID_vorbelastung){
-		
-	$tmp = mysql_query("SELECT `ID` FROM `Person`, `PersonVorbelastung` 
-						WHERE `Geburtsdatum` < '$alter[1]' 
-						AND `Geburtsdatum` > '$alter[0]' 
-						AND" . $geschlecht . $ID_vorbelastung .";");
-	while($row = mysql_fetch_object($tmp)){
-		$ID_pers[] = $row;
-	}
-	
-	return $ID_pers
-}
-
-function getWirkung($ID_pers, $ID_medikament){
-	for ($i = 0; $i < count($ID_pers); $i++) {
-		$tmp = mysql_query("SELECT `Wertung` FROM `Wirkung`,`Testergebnis` 
-							WHERE  `Wirkung.ID` = `ID_Wirkung` 
-							AND `ID_Person` = $ID_pers[$i]
-							AND `ID_Medikament` = $ID_medikament;");
-		while($row = mysql_fetch_object($tmp)){
-			$wirkung[$tmp] += 1;
-		}
-	}		
-	return $wirkung;
-
-}
-
-function getNebenwirkung($ID_pers, $ID_medikament, $ID_nebenwirkung){
-	for ($i = 0; $i < count($ID_pers); $i++) {
-		$tmp = mysql_query("SELECT `Nebenwirkung.Wertung` FROM `Nebenwirkung`,`Testergebnis`, `TestergebnisNebenwirkung`
-							WHERE  `Test_ID` = `Testergebnis.ID` 
-							AND `ID_Person` = $ID_pers[$i]
-							AND `Nebenwirkung.ID` = `Nebenwirkung_ID`
-							AND `Nebenwirkung.ID` = $ID_nebenwirkung
-							AND `ID_Medikament` = $ID_medikament;");
-		while($row = mysql_fetch_object($tmp)){
-			$nebenwirkungen[$tmp] += 1;
-		}
-	}		
-	return $nebenwirkungen;
-}
-
-$ID_pers = getPerson($alter, $geschlecht, $ID_vorbelastung)
-$wirkungen = getWirkung($ID_pers, $ID_medikament);
-$nebenWirkungen = getNebenwirkung($ID_pers, $ID_medikament, $ID_nebenwirkung);
+# Ablauf für Erstellung der Statistik
+#$ID_pers = getPerson($alter, $geschlecht, $ID_vorbelastung)
+#$wirkungen = getWirkung($ID_pers, $ID_medikament);
+#$nebenWirkungen = getNebenwirkung($ID_pers, $ID_medikament, $ID_nebenwirkung);
 ?>
