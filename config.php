b9f90511bf003a74b7965a29bd47f1ae9ee9fe27
gauurof <cihan.aktas@gmail.com>
2016-03-11 18:14:35 +0100

    config deleted

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
-
-mysql_connect("localhost","root","") or die("DB geht nicht!");
-		mysql_select_db("alcatas");
-		mysql_set_charset("UTF8");	
-?>
